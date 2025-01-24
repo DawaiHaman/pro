f9a6106e67a5c279a9209763c75e2c2cc32663b7
DawaiHaman <dhlegenie@gmail.com>
2025-01-24 20:34:01 +0100

    mon TP

--- SampleLib.h
+++ SampleLib.h
@@ -4,4 +4,16 @@
 unsigned int fact( unsigned int value );
 // Définition d'une fonction d'élévation à une puissance données.
 int power( int value, unsigned int pow );
+// Declaration de la fonction message.
+void afficherBonjour();
+//Definition de la fonction inverser.
+void inverser();
+//Definition de la fonction produit.
+void produit ();
+//Definition de la fonction parite.
+void parite ();
+//Definition de la fonction comparaison.
+void comparaison ();
+//Definition de la fonction ordre.
+void ordre ();
 #endif
--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "SampleLib.h"
+#include "T.c"
 int main( void ) {
 int result = power( 2, 4 );
 printf( "2³ == %d\n", result );
 result = fact( 5 );
 printf( "6! == %d\n", result );
+
+ afficherBonjour();
+ inverser();
+ produit ();
+ parite ();
+ comparaison ();
+ ordre ();
 return EXIT_SUCCESS;
 }
